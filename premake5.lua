881ce1a5e036998887c7866ebd15ac0901fa9389
Cohen Mejan <cohenmejan@gmail.com>
2023-11-08 02:29:39 -0500

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -2,7 +2,6 @@ project "GLFW"
 	kind "StaticLib"
 	language "C"
 	staticruntime "on"
-	warnings "off"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
